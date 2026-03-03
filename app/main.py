34fa9b8bed3a514460cc11eb39f3cebbf8097fa6
Saumya <saumyasah32@gmail.com>
2026-03-04 02:08:22 +0530

    revert last commit

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from AKS 🚀"}
+    return {"message": "Hello from AKS!"}
--- app/test_app.py
+++ app/test_app.py
@@ -3,7 +3,7 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_root():
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert "message" in response.json()
+    assert response.json() == {"message": "Hello from AKS!"}
